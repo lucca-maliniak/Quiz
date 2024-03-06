0190600ca44be870fdc16fdf0419fcd6670e758e
Lucca Maliniak <luccamaliniak@gmail.com>
2024-03-06 17:36:37 -0300

    add the function to move to next question, fix the algorithm to style the answers

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const mainSection = document.querySelector("[mainSection]")
 let quantQuestoes = 0
 let currentScore = 0
 let highScore = 0
+let index = 0
+let jsonAntigo
+let correctAnswer
+let randomQuestions
 let jsonAPI
 
 function setData(type, e) {
@@ -26,41 +30,44 @@ async function loadData() {
     })
 }
 
-async function startGame() {
+async function fetchAPI() {
     quantQuestoes = quantQuestoes == 0 ? 10 : quantQuestoes  
-    try {
-        const url = `https://opentdb.com/api.php?amount=${quantQuestoes}&category=${category}&difficulty=${difficulty}&type=multiple`
-        const response = await fetch(url)
-        const json = await response.json()
-        let randomQuestions = json.results[0].incorrect_answers
-        let correctAnswer = json.results[0].correct_answer
-        randomQuestions.push(correctAnswer)
-        randomQuestions = randomQuestions.sort()
-        mainSection.innerHTML = `
-        <div class="game-main">
-            <div class="info-game">
-                <div class="currentQuestion">
-                    Question ${0 + 1}/${quantQuestoes}    
-                </div>
-                <div class="currentScore">
-                    Current Score: ${currentScore}
-                </div>
-                <div class="highScore">
-                    High Score: ${highScore}
-                </div>
+    const url = `https://opentdb.com/api.php?amount=${quantQuestoes}&category=${category}&difficulty=${difficulty}&type=multiple`
+    const response = await fetch(url)
+    const json = await response.json()
+    return json
+}
+
+function startGame(jsonAPI) {
+    if(jsonAPI) {
+        jsonAntigo = jsonAPI
+    }
+    randomQuestions = jsonAntigo.results[index].incorrect_answers
+    correctAnswer = jsonAntigo.results[index].correct_answer
+    randomQuestions.push(correctAnswer)
+    randomQuestions = randomQuestions.sort()
+    mainSection.innerHTML = `
+    <div class="game-main">
+        <div class="info-game">
+            <div class="currentQuestion">
+                Question ${index + 1}/${quantQuestoes}    
             </div>
-            <div class="info-question">    
-                <span class="title-question">${json.results[0].question}</span>
-                <button class="btnAnswer" onclick="answerVerify(this, '${correctAnswer}')" buttonResp>${randomQuestions[0]}</button>
-                <button class="btnAnswer" onclick="answerVerify(this, '${correctAnswer}')" buttonResp>${randomQuestions[0 + 1]}</button>
-                <button class="btnAnswer" onclick="answerVerify(this, '${correctAnswer}')" buttonResp>${randomQuestions[0 + 2]}</button>
-                <button class="btnAnswer" onclick="answerVerify(this, '${correctAnswer}')" buttonResp>${randomQuestions[0 + 3]}</button>
+            <div class="currentScore">
+                Current Score: ${currentScore}
+            </div>
+            <div class="highScore">
+                High Score: ${highScore}
             </div>
         </div>
-        `
-    } catch (e) {
-        console.error("Ocorreu um erro! :( \n" + e)
-    }
+        <div class="info-question">    
+            <span class="title-question">${jsonAntigo.results[index].question}</span>
+            <button class="btnAnswer" onclick="answerVerify(this, '${correctAnswer}')" buttonResp>${randomQuestions[0]}</button>
+            <button class="btnAnswer" onclick="answerVerify(this, '${correctAnswer}')" buttonResp>${randomQuestions[0 + 1]}</button>
+            <button class="btnAnswer" onclick="answerVerify(this, '${correctAnswer}')" buttonResp>${randomQuestions[0 + 2]}</button>
+            <button class="btnAnswer" onclick="answerVerify(this, '${correctAnswer}')" buttonResp>${randomQuestions[0 + 3]}</button>
+        </div>
+    </div>
+    `
 }
 
 function answerVerify(answer, correctAnswer) {
@@ -68,6 +75,10 @@ function answerVerify(answer, correctAnswer) {
     if(answer.textContent === correctAnswer) {
         currentScore += 1000
         answer.style.backgroundColor = '#00dd00'
+        setTimeout(() => {
+            index++
+            startGame()
+        }, 800)
     } else {
         answer.style.backgroundColor = '#dd0000'
         for(let i = 0; i < arrayButtons.length; i++) {
@@ -75,6 +86,10 @@ function answerVerify(answer, correctAnswer) {
                 arrayButtons[i].style.backgroundColor = '#00dd00'
             }
         }
+        setTimeout(() => {
+            index++
+            startGame()
+        }, 800)
     }
 }
 
@@ -82,7 +97,12 @@ window.addEventListener('load', loadData)
 category.addEventListener('change', e => setData('category', e))
 nQuestions.addEventListener('change', e => setData('questions', e))
 difficulty.addEventListener('change', e => setData('difficulty', e))
-startGameBtn.addEventListener('click', startGame)
+
+startGameBtn.addEventListener('click', async e => {
+    let jsonFetchAPI = await fetchAPI()
+    console.log("json gerado pela API:", jsonFetchAPI)
+    return startGame(jsonFetchAPI)
+})
 
 
 
